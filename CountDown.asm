0c85082923020f61f7669f04259d442159d11353
Sujoy Das <sujoydas96>
2019-10-28 23:21:06 +0530

    Fully working 1-10. Possibilty for better logic

--- CountDown.asm
+++ CountDown.asm
@@ -1,22 +1,52 @@
-segment .bss
-num resb 2
+section .data
+ten db '10',0xa
+space db ' ', 0xa
 
-segment .text
-    global _start
+section	.bss
+num resb 1
 
-_start:
-    
-    mov num, 10
-    
-l1:    
-    mov edx, 2
-    mov ecx, num
-    mov ebx, 1
-    mov eax, 4
-    int 80h
-    
-    dec num
-    jnz l1
-    
-    mov eax, 1
-    int 80h
+section	.text
+   global _start        
+	
+_start:                
+
+   mov edx, 2 
+   mov ecx, ten
+   mov ebx, 1
+   mov eax, 4
+   int 0x80
+   
+   mov edx, 1 
+   mov ecx, space
+   mov ebx, 1
+   mov eax, 4
+   int 0x80
+   
+   mov ecx, 9
+   mov eax, '9'
+	
+l1:
+   mov [num], eax
+   mov eax, 4
+   mov ebx, 1
+   push ecx
+	
+   mov ecx, num        
+   mov edx, 2        
+   int 0x80
+   
+   mov edx, 1 
+   mov ecx, space
+   mov ebx, 1
+   mov eax, 4
+   int 0x80
+	
+   mov eax, [num]
+   sub eax, '0'
+   dec eax
+   add eax, '0'
+   pop ecx
+   loop l1
+	
+   mov eax,1             
+   int 0x80      
